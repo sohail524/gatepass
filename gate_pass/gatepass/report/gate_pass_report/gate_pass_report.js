4d475159a2b81889eefd17ff39966ebf2ea9fa78
sohail524 <sohailshah8018217@gmail.com>
2025-11-29 10:35:27 +0500

    Message

--- gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
+++ gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
@@ -1,6 +1,11 @@
 // Copyright (c) 2025, Sohail
 // For license information, please see license.txt
 
+/**
+ * Gate Pass Report
+ * Updated: Code cleanup + new quick filters
+ */
+
 frappe.query_reports["Gate Pass Report"] = {
     filters: [
         {
@@ -8,29 +13,41 @@ frappe.query_reports["Gate Pass Report"] = {
             label: __("Employee"),
             fieldtype: "Link",
             options: "Employee",
-            width: 150
+            width: 180,
+            description: "Filter by specific employee"
         },
         {
             fieldname: "department",
             label: __("Department"),
             fieldtype: "Link",
             options: "Department",
-            width: 150
+            width: 180,
+            description: "Filter by employee department"
         },
         {
             fieldname: "from_date",
             label: __("From Date"),
             fieldtype: "Date",
             default: frappe.datetime.add_months(
-                frappe.datetime.get_today(), 
+                frappe.datetime.get_today(),
                 -1
-            )
+            ),
+            description: "Start date of the report range"
         },
         {
             fieldname: "to_date",
             label: __("To Date"),
             fieldtype: "Date",
-            default: frappe.datetime.get_today()
+            default: frappe.datetime.get_today(),
+            description: "End date of the report range"
+        },
+        {
+            fieldname: "status",
+            label: __("Gate Pass Status"),
+            fieldtype: "Select",
+            options: "\nApproved\nRejected\nPending",
+            default: "",
+            width: 150
         }
     ]
 };
