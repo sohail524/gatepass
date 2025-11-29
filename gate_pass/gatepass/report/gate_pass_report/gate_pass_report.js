003a9f9de3c32852943accb9aba0df9489ccddf8
sohail524 <sohailshah8018217@gmail.com>
2025-11-29 10:33:04 +0500

    Message

--- gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
+++ gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
@@ -1,35 +1,36 @@
-// Copyright (c) 2025, sohail and contributors
+// Copyright (c) 2025, Sohail
 // For license information, please see license.txt
 
 frappe.query_reports["Gate Pass Report"] = {
-    "filters": [
+    filters: [
         {
-            "fieldname": "employee",
-            "label": __("Employee"),
-            "fieldtype": "Link",
-            "options": "Employee",
-            "width": 150
+            fieldname: "employee",
+            label: __("Employee"),
+            fieldtype: "Link",
+            options: "Employee",
+            width: 150
         },
         {
-            "fieldname": "department",
-            "label": __("Department"),
-            "fieldtype": "Link",
-            "options": "Department",
-            "width": 150
+            fieldname: "department",
+            label: __("Department"),
+            fieldtype: "Link",
+            options: "Department",
+            width: 150
         },
         {
-            "fieldname": "from_date",
-            "label": __("From Date"),
-            "fieldtype": "Date",
-            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
-            "reqd": 0
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.add_months(
+                frappe.datetime.get_today(), 
+                -1
+            )
         },
         {
-            "fieldname": "to_date",
-            "label": __("To Date"),
-            "fieldtype": "Date",
-            "default": frappe.datetime.get_today(),
-            "reqd": 0
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.get_today()
         }
     ]
 };
