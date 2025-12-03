ac3d23db0507573d5cb54bb2bfc9509d28b6c1ce
sohail524 <sohailshah8018217@gmail.com>
2025-12-03 11:27:08 +0500

    Message

--- gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
+++ gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
@@ -3,7 +3,7 @@
 
 /**
  * Gate Pass Report
- * Updated: Code cleanup + new quick filters
+ * Updated: Minor improvements + Added Gate Pass Type filter
  */
 
 frappe.query_reports["Gate Pass Report"] = {
@@ -13,45 +13,46 @@ frappe.query_reports["Gate Pass Report"] = {
             label: __("Employee"),
             fieldtype: "Link",
             options: "Employee",
-            width: 180,
-            description: "Filter by specific employee"
+            width: 180
         },
         {
             fieldname: "department",
             label: __("Department"),
             fieldtype: "Link",
             options: "Department",
-            width: 180,
-            description: "Filter by employee department"
+            width: 180
         },
         {
             fieldname: "from_date",
             label: __("From Date"),
             fieldtype: "Date",
-            default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
-            description: "Start date of the report range"
+            default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
         },
         {
             fieldname: "to_date",
             label: __("To Date"),
             fieldtype: "Date",
-            default: frappe.datetime.get_today(),
-            description: "End date of the report range"
+            default: frappe.datetime.get_today()
         },
         {
             fieldname: "status",
             label: __("Gate Pass Status"),
             fieldtype: "Select",
-            options: "\nApproved\nRejected\nPending",
-            default: "",
+            options: ["", "Approved", "Rejected", "Pending"].join("\n"),
+            width: 150
+        },
+        {
+            fieldname: "gate_pass_type",
+            label: __("Gate Pass Type"),
+            fieldtype: "Select",
+            options: ["", "In", "Out"].join("\n"),
             width: 150
         },
         {
             fieldname: "purpose",
-            label: __("Purpose"),
+            label: __("Purpose Contains"),
             fieldtype: "Data",
-            width: 200,
-            description: "Filter by purpose of gate pass"
+            width: 200
         }
     ]
 };
