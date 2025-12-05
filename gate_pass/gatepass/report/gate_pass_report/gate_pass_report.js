1b78f13cd84faaa0ea8e0175fcf20366cc61db59
sohail524 <sohailshah8018217@gmail.com>
2025-12-05 10:13:52 +0500

    Message

--- gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
+++ gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
@@ -3,16 +3,22 @@
 
 /**
  * Gate Pass Report
- * Update: Added Vehicle No filter + label improvements
+ * Update: Added Employee Name filter + small UI refinements
  */
 
 frappe.query_reports["Gate Pass Report"] = {
     filters: [
         {
             fieldname: "employee",
-            label: __("Employee"),
+            label: __("Employee ID"),
             fieldtype: "Link",
             options: "Employee",
+            width: 180
+        },
+        {
+            fieldname: "employee_name",
+            label: __("Employee Name Contains"),
+            fieldtype: "Data",
             width: 180
         },
         {
@@ -26,7 +32,7 @@ frappe.query_reports["Gate Pass Report"] = {
             fieldname: "vehicle_no",
             label: __("Vehicle No"),
             fieldtype: "Data",
-            width: 180
+            width: 150
         },
         {
             fieldname: "from_date",
@@ -45,14 +51,14 @@ frappe.query_reports["Gate Pass Report"] = {
             label: __("Status"),
             fieldtype: "Select",
             options: ["", "Approved", "Rejected", "Pending"].join("\n"),
-            width: 150
+            width: 140
         },
         {
             fieldname: "gate_pass_type",
             label: __("Gate Pass Type"),
             fieldtype: "Select",
             options: ["", "In", "Out"].join("\n"),
-            width: 150
+            width: 140
         },
         {
             fieldname: "purpose",
