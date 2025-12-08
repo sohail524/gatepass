c7c8a68f13ddd70df181affd6715a575f9470df4
sohail524 <sohailshah8018217@gmail.com>
2025-12-08 11:09:15 +0500

    Message

--- gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
+++ gate_pass/gatepass/report/gate_pass_report/gate_pass_report.js
@@ -3,11 +3,18 @@
 
 /**
  * Gate Pass Report
- * Update #3: Added Created By filter + layout cleanup
+ * Update #4: Added Gate Pass ID filter + filter reordering + UI polish
  */
 
 frappe.query_reports["Gate Pass Report"] = {
     filters: [
+        {
+            fieldname: "gate_pass_id",
+            label: __("Gate Pass ID"),
+            fieldtype: "Link",
+            options: "Gate Pass",
+            width: 160
+        },
         {
             fieldname: "employee",
             label: __("Employee ID"),
@@ -19,39 +26,26 @@ frappe.query_reports["Gate Pass Report"] = {
             fieldname: "employee_name",
             label: __("Employee Name Contains"),
             fieldtype: "Data",
-            width: 180
+            width: 170
         },
         {
             fieldname: "department",
             label: __("Department"),
             fieldtype: "Link",
             options: "Department",
-            width: 160
+            width: 150
         },
         {
-            fieldname: "created_by",
-            label: __("Created By (User)"),
-            fieldtype: "Link",
-            options: "User",
-            width: 160
+            fieldname: "purpose",
+            label: __("Purpose Contains"),
+            fieldtype: "Data",
+            width: 170
         },
         {
             fieldname: "vehicle_no",
             label: __("Vehicle No"),
             fieldtype: "Data",
-            width: 150
-        },
-        {
-            fieldname: "from_date",
-            label: __("From Date"),
-            fieldtype: "Date",
-            default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
-        },
-        {
-            fieldname: "to_date",
-            label: __("To Date"),
-            fieldtype: "Date",
-            default: frappe.datetime.get_today()
+            width: 140
         },
         {
             fieldname: "status",
@@ -68,10 +62,23 @@ frappe.query_reports["Gate Pass Report"] = {
             width: 130
         },
         {
-            fieldname: "purpose",
-            label: __("Purpose Contains"),
-            fieldtype: "Data",
-            width: 200
+            fieldname: "created_by",
+            label: __("Created By"),
+            fieldtype: "Link",
+            options: "User",
+            width: 150
+        },
+        {
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.add_months(frappe.datetime.get_today(), -1)
+        },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.get_today()
         }
     ]
 };
